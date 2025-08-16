b062433a05410749d2bef35b5eb0ac25e18dcd9e
leslielopez1 <leslie.lopez1@berkeley.edu>
2025-08-15 22:57:51 -0700

    Add files via upload
    This is the .py file that must be updated to change final dashboard.

---
+++ dashboard.py
@@ -0,0 +1,82 @@
+#import packages
+import streamlit as st
+import pandas as pd
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
+#import matplotlib.cm as cm
+
+#creates main section
+header = st.container()
+intro = st.container()
+data_visual = st.container()
+data_analysis = st.container()
+#summary = st.container() in case we need one last part
+
+#define the content of each section below: 
+with header: 
+    st.title("Mexico's CO\u2082 Emissions and Associations over Time")
+
+with intro:
+    st.header("Motivation")
+    st.markdown('''Mexico in the early 2000s signed the Kyoto Protocol, committing to control their greenhouse gas emissions. 
+    Since then, Mexico has introduced legislation aiming to further counter climate change, such as the Energy Transition Law. 
+    Their actions have served as an example for other developing countries.
+    Understanding how Mexico's economy heavily relies on fossil fuels for energy production, transportation, and operations, this project aims to study the change in emissions and other associations within the enviromental sector.
+    Thanks to their enviormentally-conscious president, Claudia Sheinbaum, Mexico has been taking bold strides in reducing their emissions and carbon footprint. 
+    Historically, they have been responsible for 1.6% of the world's CO2 emissions, largely from industrial fuels. This has become largely because of the market shift from an agrarian economy to more industrialized which in turn emissions went up as well.
+    With active participation in the Kyoto Protocol and Paris Agreement, they have commited to reducing their GHG emissions- from 25% to, recently updated, 35% in 2022.
+
+Citations:
+- “Mexico.” UNDP Climate Promise, climatepromise.undp.org/what-we-do/where-we-work/mexico.
+- “Mexico and Greenhouse Gas Emissions: EBSCO.” EBSCO Information Services, Inc. | Www.Ebsco.Com, www.ebsco.com/research-starters/politics-and-government/mexico-and-greenhouse-gas-emissions.
+''')
+#Main Research Qs
+    st.header("Main Research Questions")
+    st.markdown('''1) How has Mexico's CO2 emissions changed over time? And how
+        does Mexico compare to other countries (the rest of the world)?''')
+    st.markdown('''2) Are CO2 emissions, temperature, and natural disasters in Mexico associated?''')
+#Context:
+    st.header("Context")
+    st.markdown('''Comprised of multiple sources, these datasets span over different time periods and will be used to explore how CO2 emissions relate to economic, environmental, and climate indicators.
+
+Measurements:
+- **CO2 Emissions**: reflecting the average annual emissions produced by one person in each country
+- **GDP per capita**: year over year percentage change in economic output per person- how quicly the average wealth level grows
+- **Energy per person**: proxy for overall energy consumption, amount of energy used by an individual annually
+- **Disasters**: yearly rate of natural disaster events
+- **Temperature**: yearly mean temperature (in celsusis)''')
+#Limitations:
+    st.header("Limitations")
+    st.markdown('''Including CO2 emissions, the following data is a collection of different datasets which may be influenced by CO2 emissions or associated. 
+    This data allows us to examine trends within each category and compare them over the years. 
+    Additionally,  by exploring the trends we can see how enviormental and economic trends are correlated.''')
+    st.markdown('''That being said this data measures only the countries and years which they reported to various agencies, so it is not an all true  global analysis. 
+    With incomplete data, we can't draw definitive conclusions about cause and effect relationships between different categories. 
+    As the data is generalized to represent the countries as a whole, we also can not determine specific regional impacts or short-term trends.''')
+
+
+with data_visual: 
+    st.header("What is the Data?")
+    st.header("Data Visualization")
+    st.subheader("1. Country CO2 Emissions per Year (1751-2014)")
+    st.markdown("""This graph shows that Mexico is one of the countries that produces the lowest amount of CO2 emissions compared to the United States, which has dominated as the largest CO2 emission producing country until recently. 
+    Mexico's emissions have risen slightly since the year 2000.""")
+    st.subheader("2. Top 10 Emissions-producing Countries in 2010 (1900-2014)")
+    st.subheader("3. Tile Plot: Top 10 CO2 Emission-producing Countries")
+    st.subheader("4. Faceted Plot: Indicator Comparison of Mexico and the Rest of the World")
+    st.markdown("""This type of data visualization method presents how the different indicators in the dataset change through time and how they compare with each other. 
+    These plots demonstrates that each type of data spans a different time span.""")
+    st.subheader("5) Scatter Plot: CO2 Emissions and Temperature")
+    st.markdown("""These distinct scatter plots show that there are similar patterns of CO2 emission levels and average annual temperatures. 
+    There seems to be an association between high CO2 emissions and the rise of temperatures in Mexico.""")
+
+
+with data_analysis:
+    st.header("Data Analysis")
+    st.subheader("1. Calculate the Mean and SD for emissions and temperature for Mexico")
+    st.subheader("2. Calculate the correlatation coefficient for emissions and temperature")
+    st.markdown("""The correlation coefficient measures the strenght of a linear relationship between two variables. 
+    In this case, a correlatation coefficient of about 0.93 indicates a strong correlation between CO2 emissions and temperature. 
+    However, a linear relationship might not be the best way to capture the relationship, since a correlation does not impy causation.""")
+    st.subheader("3. Scaled scatter plot showing the correlation between emissions and temperature in Mexico")
