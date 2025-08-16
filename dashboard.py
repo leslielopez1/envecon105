f531a8be3b0db64756ad4629b54b4e6a4551f208
leslielopez1 <leslie.lopez1@berkeley.edu>
2025-08-16 16:05:54 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -391,6 +391,7 @@ CO2_indic_mex_facet = data_long[(data_long['Country'] == 'Mexico') & (data_long[
     (data_long['Indicator'].isin(['Emissions','Temperature','GDP','Energy','Disasters']))].drop(columns="Label").copy()
 wide_mex = CO2_indic_mex_facet.pivot(index='Year', columns='Indicator', values='Value')
 scaled_mex = wide_mex.copy()
+
 for col in scaled_mex.columns:
     scaled_mex[col] = pd.to_numeric(scaled_mex[col], errors='coerce')
 
@@ -402,15 +403,8 @@ df_clean = scaled_mex[['Emissions', choice]].dropna()
 df_clean['Emissions_scaled'] = (df_clean['Emissions'] - df_clean['Emissions'].mean()) / df_clean['Emissions'].std()
 df_clean['Indicator_scaled'] = (df_clean[choice] - df_clean[choice].mean()) / df_clean[choice].std()
 
-st.write("Emissions_scaled:")
-st.write(df_clean['Emissions_scaled'].tolist())
-st.write(f"{choice} scaled:")
-st.write(df_clean['Indicator_scaled'].tolist())
-st.write("Dtypes:")
-st.write(df_clean.dtypes)
-
-#r = np.corrcoef(df_clean['Emissions_scaled'], df_clean['Indicator_scaled'])[0,1]
-#st.write(f"Correlation coefficient between Emissions and {choice}: **{r:.2f}**")
+r = np.corrcoef(df_clean['Emissions'], df_clean['Indicator'])[0,1]
+st.write(f"Correlation coefficient between Emissions and {choice}: **{r:.2f}**")
 
 fig_corr, ax = plt.subplots(figsize=(8, 6))
 sns.regplot(df_clean, x="Emissions_scaled", y="Indicator_scaled",
